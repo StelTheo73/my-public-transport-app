dc8fa678ae56c4da804d3634a21bcb34dbc64a31
Theofilou Stylianos <stylianos.theofilou@nokia.com>
2023-11-21 10:16:11 +0200

    Use try/catch in common functions

--- src/utils/commonFunctionsDOM.js
+++ src/utils/commonFunctionsDOM.js
@@ -1,48 +1,90 @@
 export function disableElement(elementId) {
-    const element = document.getElementById(elementId);
-    element.style.filter = "blur(0.1em)";
-    element.style.cursor = "not-allowed";
-    element.setAttribute("tabindex", "-1");
-}
+    try {
+        const element = document.getElementById(elementId);
+        element.style.filter = "blur(0.1em)";
+        element.style.cursor = "not-allowed";
+        element.setAttribute("tabindex", "-1");
+    }
+    catch (error) {
+        console.debug(error.message);
+    }
+
+};
 
 export function enableElement(elementId) {
-    const element = document.getElementById(elementId);
-    element.style.filter = "";
-    element.style.cursor = "pointer";
-element.setAttribute("tabindex", "0");
-}
+    try {
+        const element = document.getElementById(elementId);
+        element.style.filter = "";
+        element.style.cursor = "pointer";
+        element.setAttribute("tabindex", "0");
+    }
+    catch (error) {
+        console.debug(error.message);
+    }
+
+};
 
 export function toggleElementVisibility(elementId, isDFlex=false) {
-    const element = document.getElementById(elementId);
-    if (element.classList.contains("hide")) {
-        element.classList.remove("hide");
+    try {
+        const element = document.getElementById(elementId);
+        console.log(element)
+        if (element.classList.contains("hide")) {
+            element.classList.remove("hide");
 
-        if (isDFlex) {
-            element.classList.add("d-flex");
+            if (isDFlex) {
+                element.classList.add("d-flex");
+            }
         }
+        else {
+            element.classList.add("hide");
+
+            if (isDFlex) {
+                element.classList.remove("d-flex");
+            }
+        }
+        console.log(element.classList)
     }
-    else {
+    catch (error) {
+        console.debug(error.message);
+    }
+};
+
+export function hideElement(elementId, isDFlex=false){
+    try {
+        const element = document.getElementById(elementId);
         element.classList.add("hide");
 
         if (isDFlex) {
             element.classList.remove("d-flex");
         }
     }
-}
+    catch (error) {
+        console.debug(error.message);
+    }
+};
 
-export function hideElement(elementId){
-    const element = document.getElementById(elementId);
-    element.classList.add("hide");
-}
+export function showElement(elementId, isDFlex=false){
+    try {
+        const element = document.getElementById(elementId);
+        element.classList.remove("hide");
 
-export function showElement(elementId){
-    const element = document.getElementById(elementId);
-    element.classList.remove("hide");
-}
+        if (isDFlex) {
+            element.classList.add("d-flex");
+        }
+    }
+    catch (error) {
+        console.debug(error.message);
+    }
+};
 
 export function removeTooltip(targetId){
-    const tooltipElement = document.getElementById(targetId);
-    tooltipElement.removeAttribute("data-tooltip-content");
+    try {
+        const tooltipElement = document.getElementById(targetId);
+        tooltipElement.removeAttribute("data-tooltip-content");
+    }
+    catch (error) {
+        console.debug(error.message);
+    }
 };
 
 export function setTooltipContent(targetId, text){
@@ -50,21 +92,31 @@ export function setTooltipContent(targetId, text){
     tooltipElement.setAttribute("data-tooltip-content", text);
 };
 
-export function markSelectedTrip(tripWrapperId, className="trip-wrapper"){
-    const otherTripWrappers = document.querySelectorAll("." + className);
-    otherTripWrappers.forEach((tripWrapper) => {
-        tripWrapper.classList.remove("trip-selected");
-    });
+export function markSelectedTrip(tripWrapperId, className="trip-wrapper") {
+    try {
+        const otherTripWrappers = document.querySelectorAll("." + className);
+        otherTripWrappers.forEach((tripWrapper) => {
+            tripWrapper.classList.remove("trip-selected");
+        });
 
-    const tripWrapper = document.getElementById(tripWrapperId);
-    tripWrapper.classList.add("trip-selected");
+        const tripWrapper = document.getElementById(tripWrapperId);
+        tripWrapper.classList.add("trip-selected");
+    }
+    catch (error) {
+        console.debug(error.message);
+    }
 };
 
 export function rotateElement(elementId) {
-    const element = document.getElementById(elementId);
+    try {
+        const element = document.getElementById(elementId);
 
-    element.classList.contains("rotate") ?
-        element.classList.remove("rotate") : element.classList.add("rotate");
-}
+        element.classList.contains("rotate") ?
+            element.classList.remove("rotate") : element.classList.add("rotate");
+    }
+    catch (error) {
+        console.debug(error.message);
+    }
+};
 
 // export function =
